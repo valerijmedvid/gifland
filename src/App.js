9e5d08778c9fd607078945ebf166a07047794cc2
valerijmedvid <vmedvid@gmail.com>
2022-08-09 19:45:14 +0200

    setup linter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import "./assets/css/App.css"
 
 function App() {
   return (
     <div className="App">
       <h1>Gifland</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
