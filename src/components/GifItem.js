c6326ec03c5f1afaf6563fc5956a939fdf3892a6
valerijmedvid <vmedvid@gmail.com>
2022-08-14 21:14:47 +0200

    add copy gifs functionality

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,5 +1,29 @@
+import { FiLink } from "react-icons/fi"
+import { BsGithub } from "react-icons/bs"
+
 function GifItem(prop) {
-  return <img src={prop.gif.media_formats.gif.url} alt="" />
+  return (
+    <div className="gifItem">
+      <div className="overlay2">
+        <span
+          onClick={() => {
+            navigator.clipboard.writeText(`![](${prop.gif.media_formats.gif.url})`)
+          }}
+        >
+          <BsGithub />
+        </span>
+        <span
+          onClick={() => {
+            navigator.clipboard.writeText(prop.gif.media_formats.gif.url)
+          }}
+        >
+          <FiLink />
+        </span>
+      </div>
+      <img src={prop.gif.media_formats.gif.url} alt={prop.gif.content_description} />
+      <div className="overlay">{prop.gif.tags.slice(0, 4).map((tag) => `#${tag} `)}</div>
+    </div>
+  )
 }
 
 export default GifItem
